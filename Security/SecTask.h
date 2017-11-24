a82d532ee83228073e0dc50256e14b3035535485
Ouroboros <Hiromi.Kaede@gmail.com>
2017-11-25 04:39:03 +0800

    add SecTask

---
+++ Security/SecTask.h
@@ -0,0 +1,54 @@
+#import <CoreFoundation/CFBase.h>
+
+CF_EXTERN_C_BEGIN
+
+/*
+
+SecTaskFinalize                     0000000180E3A2B0
+SecTaskCopyDebugDescription         0000000180E3A2DC
+SecTaskCreateFromSelf               0000000180E3A3F8    P
+SecTaskCreateWithAuditToken         0000000180E3A458    P
+SecTaskCopySigningIdentifier        0000000180E3A4B4    P
+SecTaskCopyValueForEntitlement      0000000180E3A68C    P
+SecTaskLoadEntitlements             0000000180E3A714
+SecTaskCopyValuesForEntitlements    0000000180E3A9A4    P
+
+*/
+
+typedef CFTypeRef SecTaskRef;
+
+CF_EXPORT
+CFTypeID
+SecTaskGetTypeID(
+    void
+);
+
+CF_EXPORT
+SecTaskRef
+SecTaskCreateFromSelf(
+    CFAllocatorRef allocator
+);
+
+CF_EXPORT
+SecTaskRef
+SecTaskCreateWithAuditToken(
+    CFAllocatorRef  allocator,
+    audit_token_t   token
+);
+
+CF_EXPORT
+CFTypeRef
+SecTaskCopyValueForEntitlement(
+    SecTaskRef  task,
+    CFStringRef entitlement,
+    CFErrorRef* error
+);
+
+CF_EXPORT
+CFStringRef
+SecTaskCopySigningIdentifier(
+    SecTaskRef  task,
+    CFErrorRef* error
+);
+
+CF_EXTERN_C_END
--- ml/def.h
+++ ml/def.h
@@ -3,3 +3,9 @@
         NSLog(args); \
         errno = __en; \
     }
+
+#define MSHake2(name) (void *)&$ ## name, (void **) &_ ## name
+
+#define CONST_STRLEN(x) (sizeof(x) - sizeof((x)[0]))
+
+#define IP_ADDRESS(a1, a2, a3, a4) ((a1) | ((a2) << 8) | ((a3) << 16) | ((a4) << 24))
