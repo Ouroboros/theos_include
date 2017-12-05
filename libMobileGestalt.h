cd3ac02b1098a1c9e6ef438d3edfcce86423710c
Ouroboros <Hiromi.Kaede@gmail.com>
2017-12-05 22:09:42 +0800

    MG*

--- libMobileGestalt.h
+++ libMobileGestalt.h
@@ -1,5 +1,7 @@
 CF_EXTERN_C_BEGIN
 
+BOOL MGIsQuestionValid(CFStringRef question);
+
 CFTypeRef MGCopyAnswer(CFStringRef question);
 
 class MGAnswerTypes
@@ -19,6 +21,48 @@ class MGAnswerTypes
     };
 };
 
+/*++
+
+    encodedQuestion = encoding.base64Encode(encoding.md5('MGCopyAnswer' + question))
+
+++*/
+
 CFTypeRef MGCopyAnswerInternal(CFStringRef question, int32_t* answerType);
+
+CFBooleanRef MGGetBoolAnswer(CFStringRef question);
+CFNumberRef MGGetFloat32Answer(CFStringRef question);
+CFNumberRef MGGetSInt32Answer(CFStringRef question);
+CFNumberRef MGGetSInt64Answer(CFStringRef question);
+
+int MGSetAnswer(CFStringRef question, CFTypeRef answer);
+
+void MGSetLogHandler(void (*handler)(const char* log));
+
+BOOL _MGCacheValid();
+void _MGWriteCache();
+void _MGClearInProcessCache();
+
+// MGRegisterForBulkUpdates
+
+@interface MGNotificationRegistration : NSObject {
+}
+
+- (void) cancel;
+- (void) dealloc;
+- (BOOL) start;
+// - (id) initWithQueue:(id)arg1 block:(^block)arg2;
+// - (BOOL) registerForNotification:(int)arg1 argument:(const char*)arg2 question:(id)arg3;
+
+@end
+
+MGNotificationRegistration* MGRegisterForUpdates(CFArrayRef questions, intptr_t zero1, intptr_t zero2, void (^callback)(CFStringRef question));
+
+extern CFStringRef kMGQDiskUsageAmountDataAvailable;
+extern CFStringRef kMGQDiskUsageAmountDataReserved;
+extern CFStringRef kMGQDiskUsageTotalDataAvailable;
+extern CFStringRef kMGQDiskUsageTotalDataCapacity;
+extern CFStringRef kMGQDiskUsageTotalDiskCapacity;
+extern CFStringRef kMGQDiskUsageTotalSystemAvailable;
+extern CFStringRef kMGQDiskUsageTotalSystemCapacity;
 
 CF_EXTERN_C_END
