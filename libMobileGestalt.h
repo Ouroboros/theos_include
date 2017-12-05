f30b143ab4f471a750480b4bf0727b14e77b66eb
Ouroboros <Hiromi.Kaede@gmail.com>
2017-12-05 18:39:41 +0800

    MGCopyAnswerInternal

--- libMobileGestalt.h
+++ libMobileGestalt.h
@@ -1,1 +1,24 @@
-extern "C" CFTypeRef MGCopyAnswer(CFStringRef question, CFTypeRef domain);
+CF_EXTERN_C_BEGIN
+
+CFTypeRef MGCopyAnswer(CFStringRef question);
+
+class MGAnswerTypes
+{
+    enum
+    {
+        Dictionary      = 2,
+        Array           = 3,
+        Number          = 4,
+        NumberSInt32    = 5,
+        NumberSInt64    = 6,
+        NumberFloat32   = 7,
+        NumberFloat64   = 8,
+        String          = 9,
+        Data            = 10,
+        Boolean         = 11,
+    };
+};
+
+CFTypeRef MGCopyAnswerInternal(CFStringRef question, int32_t* answerType);
+
+CF_EXTERN_C_END
