4e59a7e142c1fe4738514c5840eb280e36311513
Ouroboros <Hiromi.Kaede@gmail.com>
2017-12-08 20:44:19 +0800

    tab -> space

--- syscalls.h
+++ syscalls.h
@@ -4,6 +4,6 @@ int __open_dprotected_np(const char* path, int flags, int _class, int dpflags, i
 int __open(const char *name, int oflag, int mode);
 int __rename(const char *from, const char* to);
 int lstat64(const char *, struct stat *);
-int	ptrace(int request, pid_t pid, caddr_t addr, int data);
+int ptrace(int request, pid_t pid, caddr_t addr, int data);
 
 CF_EXTERN_C_END
