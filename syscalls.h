9097b6b987503b6554b1c6902d2885aa61aa70c5
Ouroboros <Hiromi.Kaede@gmail.com>
2018-01-10 20:25:04 +0800

    __unlink

--- syscalls.h
+++ syscalls.h
@@ -3,6 +3,7 @@ CF_EXTERN_C_BEGIN
 int __open_dprotected_np(const char* path, int flags, int _class, int dpflags, int mode);
 int __open(const char *name, int oflag, int mode);
 int __rename(const char *from, const char* to);
+int __unlink(const char *path);
 int lstat64(const char *, struct stat *);
 int ptrace(int request, pid_t pid, caddr_t addr, int data);
 
